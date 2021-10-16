88c55214622988f5ef8c62d1c4d3d6892cd94ac1
Sourabh <kalelsaurabh11@gmail.com>
2021-10-16 16:49:36 +0530

    added router parameters and breadcrumbs

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-CardTitle } from 'reactstrap';
+CardTitle, 
+Breadcrumb,
+BreadcrumbItem} from 'reactstrap';
 
     function RenderMenuItem({dish, onClick}){
         return(
             // <Card onClick={() => onClick(dish.id)}>
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                <Link to ={`menu/${dish.id}`}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         );
      
@@ -26,6 +31,16 @@ CardTitle } from 'reactstrap';
 
         return(
             <div className = "container">
+                <div className = "row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                    </div>
+                </div>
                 <div className = "row">            
                         {menu}
                 </div>
